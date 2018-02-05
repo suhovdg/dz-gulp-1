263c12514d7547679ab9bfd77df1412ad042302b
Denis Suhov <suhovdg@gmail.com>
2018-02-05 08:57:01 +0300

    Remove comments

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ gulp.task('server', ['styles'], function() {
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('./app/less/**/*.less', ['less']);
     gulp.watch('./app/less/**/*.less', ['styles']);
 });
 
